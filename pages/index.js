6acc390de4284f5c426650bbcb5d3fd5ef51bf58
matheusmenecucci23 <menecucci40@gmail.com>
2024-05-22 11:18:28 -0300

    subindo todo o projeto

---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import HomeScreen from '../src/screens/HomeScreen';
+
+export default HomeScreen;
+// import Link from '../src/components/Link';
+// import React from 'react';
+
+
+
+// function Title({ children, as }) {
+//     const Tag = as;
+//     return (
+//         <React.Fragment>
+//             <Tag>
+
+//                 {children}
+//             </Tag>
+//             <style jsx>{`
+//                 ${Tag} {
+//                     color: red;
+//                 }
+//             `}</style>
+//         </React.Fragment>
+//     );
+// }
+
+
+// export default function HomePage() {
+//     return (
+//         <div>
+//             <Title as="h2">Alura Cases - Home Page</Title>
+//             <Link href="/faq">
+//                 Ir para a página do FAQ
+//             </Link>
+//         </div>
+//     )
+// }
